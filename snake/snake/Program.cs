cf62755c3f971b7cb7860a883efe2f6d3a0eab4f
unknown <v-ivanov@V-IVANOV.chb01.rtsoft.ru>
2015-11-25 15:14:32 +0300

    Classes & methods

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,20 +10,37 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = 'x';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = 'y';
+            p2.Draw();
+
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
-            Console.SetCursorPosition(x1,y1);
-            Console.Write( sym1 );
+            Draw(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
-
+            Draw(x2, y2, sym2);*/
+         
             Console.ReadKey();
            
         }
+
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
     }
 }
