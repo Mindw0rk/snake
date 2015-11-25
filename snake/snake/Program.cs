b6398a994665d65df97694674bff0b799464d9a0
unknown <v-ivanov@V-IVANOV.chb01.rtsoft.ru>
2015-11-25 12:54:38 +0300

    Start sanke.

---
+++ snake/snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Attention!!! ");
+            Console.ReadKey();
+            Console.WriteLine("Thank you very much for your Attention!");
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
